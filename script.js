9bc3226c75db4e0ac64a4b601dc020248f818578
Bahbah89 <jesseadjomocoding@gmail.com>
2023-10-11 00:13:19 +0200

    finished project

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-'use strict'
+"use strict";
+
+const start = document.querySelector(".startContainer");
+const end = document.querySelector(".endContainer");
+
+const submitbtn = document.getElementById("submit");
+const rateAgain = document.getElementById("rate-again");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+submitbtn.addEventListener("click", function () {
+  end.classList.remove("hidden");
+  start.style.display = "none";
+});
+
+rateAgain.addEventListener("click", function () {
+  end.classList.add("hidden");
+  start.style.display = "block";
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", function () {
+    rating.innerHTML = rate.innerHTML;
+  });
+});
