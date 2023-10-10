216a53f45637d8a0784d9827b4b2c19dbd3a51b1
Bahbah89 <jesseadjomocoding@gmail.com>
2023-10-11 00:33:44 +0200

    added comments at JS script

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ const rateAgain = document.getElementById("rate-again");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
 
+// when clicking on submit button, startcontainer hides and endcontainer gets added.
 submitbtn.addEventListener("click", function () {
   end.classList.remove("hidden");
   start.style.display = "none";
 });
 
+// when clicking on rate again button, end container hides and startcontainer gets added
 rateAgain.addEventListener("click", function () {
   end.classList.add("hidden");
   start.style.display = "block";
 });
 
+// for each button, when clicked, the corresponding number will be added to the id rating in the text
 rates.forEach((rate) => {
   rate.addEventListener("click", function () {
     rating.textContent = rate.textContent;
